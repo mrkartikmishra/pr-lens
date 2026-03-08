541e264d3c128bb37d6046b41f9d9ec77d35d817
Kartik Mishra <kartikmishra.km44@gmail.com>
2026-03-08 17:16:01 +0530

    resolve coderabbit comments

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL!,
 });
 const prisma =
   globalForPrisma.prisma ||
